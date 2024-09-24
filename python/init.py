d9c90120534c3003c11361183a188e8c3ce8908c
tokeraabjerg <tokermc@hotmail.com>
2024-09-24 10:54:35 +0200

    test

--- python/init.py
+++ python/init.py
@@ -16,3 +16,34 @@
 #===========================================================================
 
 
+import addition
+
+arg1=""
+arg2=""
+
+def takeInputs():
+    arg1 = input("First number:")
+    arg2 = input("Second number:")
+
+    if(arg1.isdigit() and arg2.isdigit()):
+        print([float(arg1), float(arg2)])
+        return [float(arg1), float(arg2)]
+    else:
+        print("All arguments are not digits, please try again")
+        takeInputs()
+
+def mainScope():
+    InputArg = input("1: Addition \nX:close \n")
+
+    if(InputArg == "X"):
+        return print("Closing calculator")
+    
+    match InputArg:
+        case "1":
+            print("---- Addition ----")
+            addition.addit(takeInputs())
+        case _:
+            print("Invalid input, try again")
+            mainScope()
+
+mainScope()
